a679789dda7705c990988d8ea222a0a307de042e
jerryspann <jerryspann0@gmail.com>
2025-02-23 15:13:30 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Questions array
+
+const questions = [
+    {
+        question: 'What is the capital of Texas?',
+        options: ['Houston', 'Dallas', 'Austin', 'Humble'],
+        answer: 'Austin',
+    },
+    {
+        question: 'Who is the current President?',
+        options: ['Barak Obama', 'Hillary Clinton', 'Donald Trump', 'Elon Musk'],
+        answer: 'Donald Trump',
+    },
+    {
+        question: 'What planet is known as the "Red Planet"?',
+        options: ['Mars', 'Jupiter', 'Uranus', 'Mercury'],
+        answer: 'Mars',
+    },
+    {
+        question: 'Who was the little fairy in the cartoon Peter Pan?',
+        options: ['Wendy', 'Tinkerbell', 'Peter', 'Capton Hook'],
+        answer: 'Austin',
+    },
+]
