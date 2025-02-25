3f4930dbf692e68fa2203a1d543851932b0dae15
jerryspann <jerryspann0@gmail.com>
2025-02-24 17:01:19 -0800

    third commit

--- js/app.js
+++ js/app.js
@@ -1,24 +1,106 @@
 // Questions array
+let currentQuestion
+const testQuestions = [
+    {
+        question: 'What is the name of the MCDonalds kids meal?',
 
-const questions = [
-    {
-        question: 'What is the capital of Texas?',
-        options: ['Houston', 'Dallas', 'Austin', 'Humble'],
-        answer: 'Austin',
+        a: 'Happy Meal',
+        b: 'The Fry Guy',
+        c: 'Lunch Time Meal',
+        d: 'Unhealthy',
+
+        correct: 'Happy Meal'
     },
+
     {
-        question: 'Who is the current President?',
-        options: ['Barak Obama', 'Hillary Clinton', 'Donald Trump', 'Elon Musk'],
-        answer: 'Donald Trump',
+        question: 'What color is the sky?',
+
+        a: 'Red',
+        b: 'Yellow',
+        c: 'lLight-blue',
+        d: 'Depends on the weather',
+
+        correct: 'Depends on the weather'
     },
+
     {
-        question: 'What planet is known as the "Red Planet"?',
-        options: ['Mars', 'Jupiter', 'Uranus', 'Mercury'],
-        answer: 'Mars',
-    },
-    {
-        question: 'Who was the little fairy in the cartoon Peter Pan?',
-        options: ['Wendy', 'Tinkerbell', 'Peter', 'Capton Hook'],
-        answer: 'Austin',
+        question: 'How many cards are there in a standard deck of playing cards?',
+
+        a: 'Forty-five',
+        b: 'Fifty-two',
+        c: 'Thirteen',
+        d: 'Four',
+
+        correct: 'Fifty-two'
     },
 ]
+
+// generate question
+
+let score = 0
+
+const question = document.querySelector('#questionContainer p');
+
+// document.getElementById('answerContainer').addEventListener('click', handleChoice)
+
+
+const answersA = document.getElementById('answerA');
+const answersB = document.getElementById('answerB');
+const answersC = document.getElementById('answerC');
+const answersD = document.getElementById('answerD');
+
+answersA.addEventListener('click', handleChoice)
+answersB.addEventListener('click', handleChoice)
+answersC.addEventListener('click', handleChoice)
+answersD.addEventListener('click', handleChoice)
+
+function questionAnswer() {
+    let random = Math.floor(Math.random() * testQuestions.length)
+    let randomQuestion = testQuestions[random]
+
+    
+    console.log(randomQuestion)
+    return randomQuestion
+}
+
+function updateDisplay(randomQuestion) {
+    question.innerHTML = randomQuestion.question;
+
+    answersA.innerHTML = `${randomQuestion.a}`
+    answersB.innerHTML = `${randomQuestion.b}`
+    answersC.innerHTML = `${randomQuestion.c}`
+    answersD.innerHTML = `${randomQuestion.d}`
+
+    
+
+}
+
+
+
+
+
+console.log(currentQuestion)
+
+function handleChoice(event) {
+    let choice = event.target.textContent
+    console.log(choice)
+    let correctAnswer = currentQuestion.correct
+  
+    if (choice == correctAnswer) {
+        console.log('Good Job')
+    }
+
+}
+
+function init() {
+    currentQuestion = questionAnswer()
+    updateDisplay(currentQuestion)
+
+    
+}
+
+init()
+
+
+
+
